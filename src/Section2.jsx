ebf27971817d071474d6de0f630c2b53a965f44a
Citroll <quintinlam8@gmail.com>
2025-03-15 11:22:12 -0700

    working textbox and button with ai

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -1,27 +1,31 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { useState } from "react";
+import React, { useRef } from 'react';
 
 export function Section2() {
 
-    const [Airesponse, setAiResponse] = useState("Dog")
+    const inputRef = useRef();
+    const [Airesponse, setAiResponse] = useState("");
     const genAI = new GoogleGenerativeAI("AIzaSyCQ8QHQzPIZfn9IXZX_ePJoZzdrHiCpNH4");
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-    const run = async (e) =>{
+    const run = async (e) => {
         e.preventDefault()
-        const prompt = "how was your day";
+        const prompt = inputRef.current.value;
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
         setAiResponse(text);
     }
+
     return (
         <>
             <label>Section 3</label>
             <br></br>
-            <label htmlFor="inputbox">Write to the bot</label>
-            <input type="text" id="inputbox" name="inputbox"></input>
             <form onSubmit={run}>
+                <label htmlFor="inputbox">Write to the bot
+                    <input type="text" ref={inputRef}></input>
+                </label>
                 <button type="submit">Get response</button>
             </form>
             <br></br>
