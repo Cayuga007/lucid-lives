00e23b2c1c3e32c5642c835c658748567d422604
Citroll <quintinlam8@gmail.com>
2025-03-15 10:11:06 -0700

    implemented the api into section 2 kinda

---
+++ src/Section3.jsx
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+export function Section3() {
+    {  
+
+    }
+
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+    const run = async () =>{
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        const prompt = "tell me hello";
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        console.log(text);
+    }
+
+    run();
+    return (
+        <>
+            <label>Section 3</label>
+        </>
+    )
+}
