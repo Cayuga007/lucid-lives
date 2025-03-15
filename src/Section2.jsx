f20ac461973040753f0a0351f50b0b6143bddd95
Cayuga007 <jayson.tram13@gmail.com>
2025-03-15 11:01:20 -0700

    ai working with ui

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -1,23 +1,19 @@
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { useState } from "react";
 
 export function Section2() {
 
-    const [response, setResponse] = useState("")
+    const [Airesponse, setAiResponse] = useState("Dog")
     const genAI = new GoogleGenerativeAI("AIzaSyCQ8QHQzPIZfn9IXZX_ePJoZzdrHiCpNH4");
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
 
-    const run = async () =>{
-
+    const run = async (e) =>{
+        e.preventDefault()
         const prompt = "how was your day";
-
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
-        console.log(text);
-
-        setResponse(response);
+        setAiResponse(text);
     }
     return (
         <>
@@ -25,12 +21,13 @@ export function Section2() {
             <br></br>
             <label htmlFor="inputbox">Write to the bot</label>
             <input type="text" id="inputbox" name="inputbox"></input>
-            <form onSubmit ={run}>
+            <form onSubmit={run}>
                 <button type="submit">Get response</button>
             </form>
             <br></br>
 
-            <output name="out" htmlFor="response">{response}</output>
+            <label>{Airesponse}</label>
+            <output name="out" htmlFor="response"></output>
         </>
     )
 }
