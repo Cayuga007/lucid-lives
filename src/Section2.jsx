88e0e0a4cc3c130c50e70963138b16adadb0bac7
Citroll <quintinlam8@gmail.com>
2025-03-15 13:29:34 -0700

    problemo with the api

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -4,19 +4,61 @@ import React, { useRef } from 'react';
 
 export function Section2() {
 
+    /*const careerQuestions = [
+        "Can you describe your passion for your career?",
+        "What did you want to be as a child?",
+        "Where do you see yourself in five years?",
+        "What gives you the most stress?",
+        "What are some risks you’ve taken that have impacted your life?"
+    ];
+
+    const relationQuestions = [
+        "How does love make you feel?",
+        "What do you feel when the time comes to say goodbye to someone?",
+        "What is a day that you wish you could go back to, if there is one?",
+        "What is your biggest fear?",
+        "How often do you form meaningful connections?",
+        "Describe a time when you felt truly understood by someone?",
+        "How has a past connection shaped who you are today?"
+    ];*/
+
+    const emotions = {
+        diffCareer: 0,
+        currCareer: 0,
+        romance: 0,
+        friendship: 0,
+    }
+
+    const question = "are you happy with your career, or do you think you could've done better";
+
     const inputRef = useRef();
-    const [Airesponse, setAiResponse] = useState("");
-    const genAI = new GoogleGenerativeAI("AIzaSyCQ8QHQzPIZfn9IXZX_ePJoZzdrHiCpNH4");
+    const [Airesponse, setAiResponse] = useState(question);
+    const genAI = new GoogleGenerativeAI("AIzaSyAMXXAWbcURO3KB58f78hpgOOyg4toXgac");
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
     const run = async (e) => {
         e.preventDefault()
         const prompt = inputRef.current.value;
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = response.text();
-        setAiResponse(text);
+
+        const categorize = async (prompt) => {
+            const aiPrompt = "reply with poop if i say hello, otherwise say dog, me:" + prompt;
+
+            try {
+                const result = await model.generateContent(aiPrompt);
+                const response = await result.response;
+                const text = response.text();
+                setAiResponse(text);
+            } catch (error) {
+                if (error.response && error.response.status === 429) {
+                    console.error("too many requests, wait or check ur quota");
+                } else {
+                    console.error("an error occured: ", error);
+                }
+            }
+        }
+        categorize(prompt);
     }
+
 
     return (
         <>
